a31726a079f54e2774b4cdf94ed7529406eb7fb5
ubaidmanzoor987 <ubaidmanzoor987@gmail.com>
2025-04-07 13:01:04 +0500

    added key

--- app.py
+++ app.py
@@ -6,6 +6,4 @@ app=Flask(__name__)
 def home():
     return "hello world this is working fine"
 if __name__ == "__main__":
-    app.run(debug=True) 
-
-    
+    app.run(host="0.0.0.0", port=5000, debug=True)
